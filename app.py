c8e38a7a34aa85669e67ae0ce9c694883e8c2c84
sindhu-anu <sindhu3136@gmail.com>
2026-03-05 01:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,6 @@
-from flask import Flask, request, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def calculator():
-    result = None
-    if request.method == "POST":
-        a = float(request.form["a"])
-        b = float(request.form["b"])
-        op = request.form["op"]
-
-        if op == "+":
-            result = a + b
-        elif op == "-":
-            result = a - b
-        elif op == "*":
-            result = a * b
-        elif op == "/":
-            result = "Error" if b == 0 else a / b
-
-    return render_template("index.html", result=result)
-
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def hello():
+    return "Hello, World!"
